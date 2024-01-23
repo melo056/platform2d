840471911d984322654c7dc51c30b59504138035
melo056 <genesis66ita@gmail.com>
2024-01-23 18:38:49 +0100

    script movement

--- platform2d/Assets/script/movement.cs
+++ platform2d/Assets/script/movement.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-   
+    private Rigidbody2D rb;
+    
+    public float speed;
+    private float Move;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+       
     }
     void Movement()
     {
-        
-           
-        
+        Move = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
     }
+   
 }
