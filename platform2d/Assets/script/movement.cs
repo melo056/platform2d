bea9a2913d43be3cbff081e372f47acccfbe0ffe
melo056 <genesis66ita@gmail.com>
2024-01-24 02:06:26 +0100

    jump e groundcheck

--- platform2d/Assets/script/movement.cs
+++ platform2d/Assets/script/movement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
+    public Transform GroundCheck;
+
     public float speed;
+    public float jumpspeed = 4f;
     private float Move;
+    public float groundcheckradius;
+    public LayerMask groundLayer;
+    public bool isTouchingGround;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,7 @@ public class movement : MonoBehaviour
     void Update()
     {
         Movement();
+        Jump();
        
     }
     void Movement()
@@ -26,5 +32,15 @@ public class movement : MonoBehaviour
         Move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(Move * speed, rb.velocity.y);
     }
-   
+
+    void Jump()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(GroundCheck.position, groundcheckradius, groundLayer);
+        if (Input.GetButtonDown("Jump") && isTouchingGround == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+            
+        }
+    } 
+    
 }
